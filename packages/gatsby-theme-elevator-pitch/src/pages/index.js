042a469b191e9d0b68b1861dd7721f69dad44654
greglobinski <greglobinski@gmail.com>
2019-02-11 01:28:31 +0100

    test02

--- packages/gatsby-theme-elevator-pitch/src/pages/index.js
+++ packages/gatsby-theme-elevator-pitch/src/pages/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 import Viewer from '../components/Viewer';
 
-const IndexPage = props => <Viewer />;
+const IndexPage = props => <div>asdfasdfas</div>;
 
 export default IndexPage;
